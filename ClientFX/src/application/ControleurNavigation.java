ba807fb27a42e056b5c2e52635c8a5e7f7e8ff05
asorin.alexia <asorin.alexia@gmail.com>
2022-03-09 15:32:32 +0100

    début interface navigation

---
+++ ClientFX/src/application/ControleurNavigation.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+
+public class ControleurNavigation implements Initializable {
+  @FXML
+  private TextField inputCommande;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
